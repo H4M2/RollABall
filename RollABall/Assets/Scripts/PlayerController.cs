c9eed5dbbda18247cb1f9e67a63b32ef07444f11
FrogInMySock <Rman434@gmail.com>
2022-10-12 23:19:01 +0100

    Latest version, no pickup score

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -3,40 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
-{
+
+public class PlayerController : MonoBehaviour {
 
     public float speed;
     private int count;
     private int numPickups = 8; // Put here the number of pickups you have .
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI winText;
+    public Vector2 moveValue ;
 
-    void Start() 
-    {
+    void OnMove ( InputValue value ) {
+        moveValue = value.Get<Vector2>() ;
+    }
+
+    void FixedUpdate () {
+        Vector3 movement = new Vector3 (moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void Start() {
         count = 0;
         winText.text = "";
         SetCountText ();
     }
 
-    void FixedUpdate() {} // NO changes
+    void Update () {}
+ 
+    //void FixedUpdate() {} // NO changes
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == " PickUp ")
-        {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "PickUp") {
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
         }
     }
 
-    private void SetCountText()
-    {
+    private void SetCountText() {
         scoreText.text = "Score: " + count.ToString();
-        if (count >= numPickups)
-        {
+        if (count >= numPickups) {
             winText.text = "You win!";
         }
     }
