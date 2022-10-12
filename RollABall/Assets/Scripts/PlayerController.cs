c3408bf1e1d6f24b3d83790063afd08fb00c635b
H4M2 <mdhahad10@gmail.com>
2022-10-12 22:32:52 +0100

    Scripting time

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -1,33 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public Vector2 moveValue;
-	public float speed;
-	private int count;
+    public float speed;
+    private int count;
+    private int numPickups = 8; // Put here the number of pickups you have .
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
-	void OnMove ( InputValue value ) {
-		moveValue = value.Get<Vector2>();
-	}
+    void Start() 
+    {
+        count = 0;
+        winText.text = "";
+        SetCountText ();
+    }
 
-void Start (){
-	count = 0;
-}
+    void FixedUpdate() {} // NO changes
 
-void OnTriggerEnter (Collider other) {
-	if(other.gameObject.tag == "PickUp") {
-		other.gameObject.SetActive(false);
-		count ++;
-	}
-}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == " PickUp ")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
 
-
-void FixedUpdate () {
-	Vector3 movement = new Vector3 (moveValue.x, 0.0f, moveValue.y);
-	
-	GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
-	}
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = "You win!";
+        }
+    }
 }
